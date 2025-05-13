0e5b81bc31d303fe020fa8f0dadf5b4f79ca4ff1
Felipe Wagner da Silva <fws.buss@gmail.com>
2025-05-13 13:16:38 -0300

    Exercicio 13 e 14

---
+++ Exercicios/ProgDesc/AlocacaoDinamica/EX14/ex14.c
@@ -0,0 +1,14 @@
+/*
+Construa um programa que leia o numero de linhas e de colunas de uma matriz de
+numeros reais, aloque espaco dinamicamente para esta e a inicialize com valores 
+fornecidos pelo usuario. Ao final, o programa devera retornar a matriz na saida padrao com
+layout apropriado.
+*/
+
+#include <stdio.h>
+#include <stdio.h>
+
+int main(){
+    
+
+}
