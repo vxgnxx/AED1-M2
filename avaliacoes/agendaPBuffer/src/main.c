e32919e7815ce5a639eac10ad644e9c8dd3ed2cc
Felipe Wagner da Silva <fws.buss@gmail.com>
2025-06-11 13:58:39 -0300

    Buffer add

--- avaliacoes/agendaPBuffer/src/main.c
+++ avaliacoes/agendaPBuffer/src/main.c
@@ -2,13 +2,19 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(int argc, char const *argv[])
-{
-    void *pBuffer = malloc(sizeof(int));
+typedef int nameSize_t;
+typedef int age_t;
+typedef int emailSize_t;
 
+void addPerson(void **pBuffer);
+
+int main(){
+
+    void *pBuffer = malloc(216);
     *(int *)pBuffer = 0;
 
-    int *opcao = (int *)((char *)pBuffer + sizeof(int));
+    int *opcao = malloc(sizeof(int));
+    *opcao = 0;
 
     do {
         printf("\nAGENDA\n");
@@ -24,7 +30,7 @@ int main(int argc, char const *argv[])
         
         switch (*opcao) {
             case 1:
-                //adicionarPessoa(pBuffer);
+                addPerson(&pBuffer);
                 printf("\nadd\n");
                 break;
             case 2:
@@ -49,4 +55,77 @@ int main(int argc, char const *argv[])
 
     free(pBuffer);
     return 0;
+}
+
+void addPerson(void **pBuffer){
+    void *p = *pBuffer;
+
+    int  *quantity  = (int *) (p);        // offset 0
+    int  *nameSize  = (int *) (p + 4);    // offset 4
+    int  *age       = (int *) (p + 8);    // offset 8
+    int  *emailSize = (int *) (p + 12);   // offset 12
+    char *tempName  = (char *)(p + 16);   // offset 16
+    char *tempEmail = (char *)(p + 116);  // offset 116
+
+    // Input
+    printf("Nome: ");
+    fgets(tempName, 100, stdin);
+    tempName[strcspn(tempName, "\n")] = '\0';
+
+    printf("Idade: ");
+    scanf("%d", age);
+    getchar();
+
+    printf("Email: ");
+    fgets(tempEmail, 100, stdin);
+    tempEmail[strcspn(tempEmail, "\n")] = '\0';
+
+    *nameSize = strlen(tempName) + 1;
+    *emailSize = strlen(tempEmail) + 1;
+
+    int newPersonSize = sizeof(int) + *nameSize + sizeof(int) + sizeof(int) + *emailSize;
+    int offset = 216; // Start of person data
+    char *data = (char *)(p + 216);
+
+    for (int i = 0; i < *quantity; i++) {
+        int currNameSize = *(int *)data;
+        data += sizeof(int) + currNameSize;  // nameSize + name
+        data += sizeof(int);                 // age
+        int currEmailSize = *(int *)data;
+        data += sizeof(int) + currEmailSize; // emailSize + email
+        offset = data - (char *)p;
+    }
+
+    *pBuffer = realloc(*pBuffer, offset + newPersonSize);
+
+    // Update pointers after realloc
+    p = *pBuffer;
+    quantity =  (int *) (p);
+    nameSize =  (int *) (p + 4);
+    age =       (int *) (p + 8);
+    emailSize = (int *) (p + 12);
+    tempName =  (char *)(p + 16);
+    tempEmail = (char *)(p + 116);
+
+    data = (char *)p + offset;
+
+    *(int *)data = *nameSize;
+    data += sizeof(int);
+    memcpy(data, tempName, *nameSize);
+    data += *nameSize;
+
+    *(int *)data = *age;
+    data += sizeof(int);
+
+    *(int *)data = *emailSize;
+    data += sizeof(int);
+    memcpy(data, tempEmail, *emailSize);
+
+    (*quantity)++;
+}
+
+void removePerson(void **pBuffer){
+
+
+
 }
